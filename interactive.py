424cba42e99bc2e5ad7d983bc1c7e6c441fd9632
Kenneth Bradfute <26446792+kbradfute@users.noreply.github.com>
2018-01-01 11:50:10 -0700

    Update interactive.py

--- interactive.py
+++ interactive.py
@@ -9,17 +9,17 @@ name = input('Hello, who am I working with today? ')
 
 print('\n' + 'Great to see you ' + name.capitalize() + '!' + ' Let\'s get some work done.')
 
-if date == '0':
+if date == 0:
     print('Today is Monday and it is currently ' + time.strftime("%H:%M"))
-elif date == '1':
+elif date == 1:
     print('Today is Tuesday and it is currently ' + time.strftime("%H:%M"))
-elif date == '2':
+elif date == 2:
     print('Today is Wednsday and it is currently ' + time.strftime("%H:%M"))
-elif date == '3':
+elif date == 3:
     print('Today is Thursday and it is currently ' + time.strftime("%H:%M"))
-elif date == '4':
+elif date == 4:
     print('Today is Friday and it is currently ' + time.strftime("%H:%M"))
-elif date == '5':
+elif date == 5:
     print('Today is Saturday and it is currently ' + time.strftime("%H:%M"))
 else:
     print('Today is Sunday and it is currently ' + time.strftime("%H:%M") + '.')
