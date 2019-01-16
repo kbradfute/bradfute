7ecb138ed2e54c564585ad4b47ebd3ad3c844c10
Kenneth Bradfute <26446792+kbradfute@users.noreply.github.com>
2019-01-15 17:08:17 -0700

    Update Block_IP.py
    Removed typo in device list.

--- Block_IP.py
+++ Block_IP.py
@@ -22,7 +22,7 @@ block = raw_input("What IP address would you like to block? ")
 
 config_set = ['object-group network THE_BAD_GUYS2', 'network-object host {block}'.format(**locals()), 'end']
 
-for asa in (asas1, asa2):
+for asa in (asa1, asa2):
     with ConnectHandler(**asa) as device:
         device.enable()
         device.config_mode()
