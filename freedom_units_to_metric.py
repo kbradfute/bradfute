2ec951aa227810487211ea223133d5f2733da0b0
Kenneth Bradfute <26446792+kbradfute@users.noreply.github.com>
2021-08-21 11:30:45 -0700

    Create freedom_units_to_metric.py

---
+++ freedom_units_to_metric.py
@@ -0,0 +1,15 @@
+cmPerInch = 2.54
+inchesPerFeet = 12
+
+def HeightFtInToCm(heightFt,heightIn):
+    float.cmPerInch
+    int.inchesPerFeet()
+    int.totalInches()
+    totalInches = (heightFt * inchesPerFeet) + heightIn
+    heightCm = totalInches * cmPerInch
+    return heightCm
+
+heightFt = input("How tall are you in feet?: ")
+heightIn = input("How tall are you in inches?: ")
+
+print("You are",heightCm," centimeters tall!")
